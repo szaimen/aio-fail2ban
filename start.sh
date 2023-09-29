2b8668273f8ff2da78513943f2de6c9eb589cbf9
Simon L <szaimen@e.mail.de>
2023-09-29 16:00:01 +0200

    add vaultwarden
    Signed-off-by: Simon L <szaimen@e.mail.de>

--- start.sh
+++ start.sh
@@ -14,6 +14,9 @@ while ! [ -f /nextcloud/data/nextcloud.log ]; do
 done
 
 cat << FILTER > /etc/fail2ban/filter.d/nextcloud.conf
+[INCLUDES]
+before = common.conf
+
 [Definition]
 _groupsre = (?:(?:,?\s*"\w+":(?:"[^"]+"|\w+))*)
 failregex = ^\{%(_groupsre)s,?\s*"remoteAddr":"<HOST>"%(_groupsre)s,?\s*"message":"Login failed:
@@ -23,16 +26,69 @@ FILTER
 
 cat << JAIL > /etc/fail2ban/jail.d/nextcloud.local
 [nextcloud]
-backend = auto
 enabled = true
 port = 80,443,8080,8443,3478
 protocol = tcp,udp
 filter = nextcloud
-maxretry = 10
-bantime = 86400
-findtime = 43200
+banaction = %(banaction_allports)s
+maxretry = 3
+bantime = 14400
+findtime = 14400
 logpath = /nextcloud/data/nextcloud.log
 chain=DOCKER-USER
 JAIL
+
+if [ -f /vaultwarden/vaultwarden.log ]; then
+    echo "Configuring vaultwarden for logs"
+    # Vaultwarden conf
+    cat << BW_CONF > /etc/fail2ban/filter.d/vaultwarden.conf
+[INCLUDES]
+before = common.conf
+
+[Definition]
+failregex = ^.*Username or password is incorrect\. Try again\. IP: <ADDR>\. Username:.*$
+ignoreregex =
+BW_CONF
+
+    # Vaultwarden jail
+    cat << BW_JAIL_CONF > /etc/fail2ban/jail.d/vaultwarden.local
+[vaultwarden]
+enabled = true
+port = 80,443,8812
+protocol = tcp,udp
+filter = vaultwarden
+banaction = %(banaction_allports)s
+logpath = /vaultwarden/vaultwarden.log
+maxretry = 3
+bantime = 14400
+findtime = 14400
+chain=DOCKER-USER
+BW_JAIL_CONF
+
+    # Vaultwarden-admin conf
+    cat << BWA_CONF > /etc/fail2ban/filter.d/vaultwarden-admin.conf
+[INCLUDES]
+before = common.conf
+
+[Definition]
+failregex = ^.*Invalid admin token\. IP: <ADDR>.*$
+ignoreregex =
+BWA_CONF
+
+    # Vaultwarden-admin jail
+    cat << BWA_JAIL_CONF > /etc/fail2ban/jail.d/vaultwarden-admin.local
+[vaultwarden-admin]
+enabled = true
+port = 80,443,8812
+protocol = tcp,udp
+filter = vaultwarden-admin
+banaction = %(banaction_allports)s
+logpath = /vaultwarden/vaultwarden.log
+maxretry = 3
+bantime = 14400
+findtime = 14400
+chain=DOCKER-USER
+BWA_JAIL_CONF
+fi
 
 fail2ban-server -f --logtarget stderr --loglevel info 
