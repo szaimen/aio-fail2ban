e0b92285f10638d5f0f48cf5aa16942f29865f05
Simon L. <szaimen@e.mail.de>
2026-02-06 10:16:03 +0100

    adjust fail2ban chain to also write to default input chain
    Signed-off-by: Simon L. <szaimen@e.mail.de>

--- start.sh
+++ start.sh
@@ -24,8 +24,8 @@ failregex = ^\{%(_groupsre)s,?\s*"remoteAddr":"<HOST>"%(_groupsre)s,?\s*"message
 datepattern = ,?\s*"time"\s*:\s*"%%Y-%%m-%%d[T ]%%H:%%M:%%S(%%z)?"
 FILTER
 
-cat << JAIL > /etc/fail2ban/jail.d/nextcloud.local
-[nextcloud]
+cat << JAIL > /etc/fail2ban/jail.d/nextcloud-host.local
+[nextcloud-host]
 enabled = true
 port = 80,443,8080,8443,3478
 protocol = tcp,udp
@@ -35,9 +35,14 @@ maxretry = 3
 bantime = 14400
 findtime = 14400
 logpath = /nextcloud/data/nextcloud.log
-chain=DOCKER-USER
+# chain=DOCKER-USER
 ignoreip = 127.0.0.1/8 192.168.0.0/16 172.16.0.0/12 10.0.0.0/8 fd00::/8 ::1
 JAIL
+
+rm -f /etc/fail2ban/jail.d/nextcloud.local
+cp /etc/fail2ban/jail.d/nextcloud-host.local /etc/fail2ban/jail.d/nextcloud.local
+sed -i "s|\[nextcloud-host\]|\[nextcloud\]|" /etc/fail2ban/jail.d/nextcloud.local
+sed -i "s|^# chain|chain|" /etc/fail2ban/jail.d/nextcloud.local
 
 if [ -f /vaultwarden/vaultwarden.log ]; then
     echo "Configuring vaultwarden for logs"
@@ -52,8 +57,8 @@ ignoreregex =
 BW_CONF
 
     # Vaultwarden jail
-    cat << BW_JAIL_CONF > /etc/fail2ban/jail.d/vaultwarden.local
-[vaultwarden]
+    cat << BW_JAIL_CONF > /etc/fail2ban/jail.d/vaultwarden-host.local
+[vaultwarden-host]
 enabled = true
 port = 80,443,8812
 protocol = tcp,udp
@@ -63,9 +68,14 @@ logpath = /vaultwarden/vaultwarden.log
 maxretry = 3
 bantime = 14400
 findtime = 14400
-chain=DOCKER-USER
+# chain=DOCKER-USER
 ignoreip = 127.0.0.1/8 192.168.0.0/16 172.16.0.0/12 10.0.0.0/8 fd00::/8 ::1
 BW_JAIL_CONF
+
+    rm -f /etc/fail2ban/jail.d/vaultwarden.local
+    cp /etc/fail2ban/jail.d/vaultwarden-host.local /etc/fail2ban/jail.d/vaultwarden.local
+    sed -i "s|\[vaultwarden-host\]|\[vaultwarden\]|" /etc/fail2ban/jail.d/vaultwarden.local
+    sed -i "s|^# chain|chain|" /etc/fail2ban/jail.d/vaultwarden.local
 
     # Vaultwarden-admin conf
     cat << BWA_CONF > /etc/fail2ban/filter.d/vaultwarden-admin.conf
@@ -78,8 +88,8 @@ ignoreregex =
 BWA_CONF
 
     # Vaultwarden-admin jail
-    cat << BWA_JAIL_CONF > /etc/fail2ban/jail.d/vaultwarden-admin.local
-[vaultwarden-admin]
+    cat << BWA_JAIL_CONF > /etc/fail2ban/jail.d/vaultwarden-admin-host.local
+[vaultwarden-admin-host]
 enabled = true
 port = 80,443,8812
 protocol = tcp,udp
@@ -89,9 +99,14 @@ logpath = /vaultwarden/vaultwarden.log
 maxretry = 3
 bantime = 14400
 findtime = 14400
-chain=DOCKER-USER
+# chain=DOCKER-USER
 ignoreip = 127.0.0.1/8 192.168.0.0/16 172.16.0.0/12 10.0.0.0/8 fd00::/8 ::1
 BWA_JAIL_CONF
+
+    rm -f /etc/fail2ban/jail.d/vaultwarden-admin.local
+    cp /etc/fail2ban/jail.d/vaultwarden-admin-host.local /etc/fail2ban/jail.d/vaultwarden-admin.local
+    sed -i "s|\[vaultwarden-admin-host\]|\[vaultwarden-admin\]|" /etc/fail2ban/jail.d/vaultwarden-admin.local
+    sed -i "s|^# chain|chain|" /etc/fail2ban/jail.d/vaultwarden-admin.local
 fi
 
 if [ -d /jellyfin/log ]; then
@@ -106,8 +121,8 @@ failregex = ^.*Authentication request for .* has been denied \(IP: "<ADDR>"\)\.
 JELLYFIN_CONF
 
     # Jellyfin jail
-    cat << JELLYFIN_JAIL_CONF > /etc/fail2ban/jail.d/jellyfin.local
-[jellyfin]
+    cat << JELLYFIN_JAIL_CONF > /etc/fail2ban/jail.d/jellyfin-host.local
+[jellyfin-host]
 enabled = true
 port = 80,443,8096,8920,1900,7359
 protocol = tcp,udp
@@ -117,9 +132,14 @@ maxretry = 3
 bantime = 86400
 findtime = 43200
 logpath = /jellyfin/log/*.log
-chain=DOCKER-USER
+# chain=DOCKER-USER
 ignoreip = 127.0.0.1/8 192.168.0.0/16 172.16.0.0/12 10.0.0.0/8 fd00::/8 ::1
 JELLYFIN_JAIL_CONF
+
+    rm -f /etc/fail2ban/jail.d/jellyfin.local
+    cp /etc/fail2ban/jail.d/jellyfin-host.local /etc/fail2ban/jail.d/jellyfin.local
+    sed -i "s|\[jellyfin-host\]|\[jellyfin\]|" /etc/fail2ban/jail.d/jellyfin.local
+    sed -i "s|^# chain|chain|" /etc/fail2ban/jail.d/jellyfin.local
 fi
 
 if [ -d /jellyseerr/logs ]; then
@@ -134,8 +154,8 @@ failregex = .*\[warn\]\[API\]\: Failed sign-in attempt.*"ip":"<HOST>"
 JELLYSEERR_CONF
 
     # Jellyseerr jail
-    cat << JELLYSEERR_JAIL_CONF > /etc/fail2ban/jail.d/jellyseerr.local
-[jellyseerr]
+    cat << JELLYSEERR_JAIL_CONF > /etc/fail2ban/jail.d/jellyseerr-host.local
+[jellyseerr-host]
 enabled = true
 port = 80,443,5055
 protocol = tcp,udp
@@ -145,9 +165,14 @@ maxretry = 3
 bantime = 86400
 findtime = 43200
 logpath = /jellyseerr/logs/*.log
-chain=DOCKER-USER
+# chain=DOCKER-USER
 ignoreip = 127.0.0.1/8 192.168.0.0/16 172.16.0.0/12 10.0.0.0/8 fd00::/8 ::1
 JELLYSEERR_JAIL_CONF
+
+    rm -f /etc/fail2ban/jail.d/jellyseerr.local
+    cp /etc/fail2ban/jail.d/jellyseerr-host.local /etc/fail2ban/jail.d/jellyseerr.local
+    sed -i "s|\[jellyseerr-host\]|\[jellyseerr\]|" /etc/fail2ban/jail.d/jellyseerr.local
+    sed -i "s|^# chain|chain|" /etc/fail2ban/jail.d/jellyseerr.local
 fi
 
 fail2ban-server -f --logtarget stderr --loglevel info 
